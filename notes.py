af3b5ffe57dd3f4eb30b37946adcbea464c00488
aurora <aurorawibowo@hotmail.com>
2025-08-22 11:14:12 -0400

    Main

---
+++ notes.py
@@ -0,0 +1,19 @@
+# def is the keuwprd tp deome a fimctopm
+# whatType is the name of the function
+# userInput is the parameter of the function
+
+def whatType(userInput):
+    # print is a Python built-in function that prints to the console
+    # type is a Python biult-in function that finds the datatype
+    # userInput is the variable that the user enters
+    print(type(userInput))
+
+"""
+multiple lines 
+"""
+
+whatType(3)
+whatType(True)
+whatType("aurora")
+whatType("p")
+whatType(3.0)
